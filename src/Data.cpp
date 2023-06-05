94dff03a886f26af793ecdacdf508234836c1cd2
ilya Pashchenko <in4pashchenko@gmail.com>
2023-06-05 18:36:34 +0300

    Each 5-th point to speed up debugging.

--- src/Data.cpp
+++ src/Data.cpp
@@ -25,9 +25,18 @@ void Data::load(std::string filename)
 	}
 	fin.close();
 	
-	r = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(r_.data(), r_.size());
-	R = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(R_.data(), R_.size());
-	r = r(Eigen::seq(0, Eigen::last, 5));
-	R = R(Eigen::seq(0, Eigen::last, 5));
+	std::vector<double> r__, R__;
+	int size = r_.size();
+	for(int i=0; i < size; i += 5)
+	{
+		r__.push_back(r_[i]);
+		R__.push_back(R_[i]);
+	}
+	
+	r = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(r__.data(), r__.size());
+	R = Eigen::Map<Eigen::VectorXd, Eigen::Unaligned>(R__.data(), R__.size());
+	// This makes first r 1E-512
+//	r = r(Eigen::seq(0, Eigen::last, 2));
+//	R = R(Eigen::seq(0, Eigen::last, 2));
 }
 
