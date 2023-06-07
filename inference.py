07e5b92c0bf629150db4ac216b07ccad86737c54
ilya Pashchenko <in4pashchenko@gmail.com>
2023-06-07 20:31:07 +0300

    Making inference from the observed z1.

---
+++ inference.py
@@ -0,0 +1,131 @@
+import matplotlib
+matplotlib.use("TkAgg")
+import numpy as np
+from astropy import units as u, constants as const, cosmology
+from scipy.stats import median_abs_deviation
+import functools
+import matplotlib.pyplot as plt
+
+cosmo = cosmology.WMAP9
+
+# Speed of light [cm / s]
+c = 29979245800.0
+# Gravitational constant
+G = 6.67430e-08
+# Parsec [cm]
+pc = 3.0856775814671913e+18
+# Mass of the Sun [g]
+M_sun = 1.98840987e+33
+
+@functools.lru_cache()
+def ang_to_dist(z):
+    return cosmo.kpc_proper_per_arcmin(z)
+
+
+@functools.lru_cache()
+def mas_to_pc(l_mas, z, theta_obs):
+    """
+    Given some observed length ``l_mas`` [mas] it returns corresponding de-projected length in pc for a given redshift
+    ``z`` and jet viewing angle ``theta_obs`` [rad].
+    """
+    return (l_mas*u.mas*ang_to_dist(z)).to(u.pc).value/np.sin(theta_obs)
+
+@functools.lru_cache()
+def get_R_g(M_BH):
+    """
+    Returns R_g [cm].
+    """
+    return G*M_BH/c/c
+
+def get_R_horizon(M_BH, a):
+    """
+    Returns R_h [cm].
+    """
+    return get_R_g(M_BH)*(1.0 + np.sqrt(1.0 - a*a))
+
+def get_R_L(M_BH, a):
+    """
+    Returns R_L [cm].
+    """
+    return 4.0*get_R_horizon(M_BH, a)/a
+
+def get_z1_Ma(gamma_in, sigma_M, M_BH_sun, a, theta_jet=None):
+    """
+    The de-projected (real, not observed) position of the break.
+
+    :param gamma_in:
+        Initial Lorenz factor of the bulk motion.
+    :param sigma_M:
+        Initial magnetization.
+    :param M_BH_sun:
+        Black hole mass [Sun masses].
+    :param a:
+        Spin of the black hole.
+    :param theta_jet: (optional)
+        Conical half-angle [rad]. If ``None`` then use expression for the parabolic case. (default: ``None``)
+    :return:
+        The distance to the break position in pc.
+    """
+    R_L = get_R_L(M_BH_sun*M_sun, a)
+    # Parabolic case
+    if theta_jet is None:
+        return gamma_in*sigma_M*R_L/pc
+    # Conical case
+    else:
+        return np.sqrt(gamma_in*sigma_M)*R_L/theta_jet/pc
+
+
+def get_z1_RL(gamma_in, sigma_M, R_L, theta_jet=None):
+    """
+    The de-projected (real, not observed) position of the break.
+
+    :param gamma_in:
+        Initial Lorenz factor of the bulk motion.
+    :param sigma_M:
+        Initial magnetization.
+    :param R_L:
+        Light cylinder radius [pc].
+    :param theta_jet: (optional)
+        Conical half-angle [rad]. If ``None`` then use expression for the parabolic case. (default: ``None``)
+    :return:
+        The distance to the break position in pc.
+    """
+    # Parabolic case
+    if theta_jet is None:
+        return gamma_in*sigma_M*R_L/pc
+    # Conical case
+    else:
+        return np.sqrt(gamma_in*sigma_M)*R_L/theta_jet/pc
+
+
+if __name__ == "__main__":
+    z = 0.00436
+    D = 16.8e+06*u.pc
+    sigma_D = 0.8e+06*u.pc
+    M_BH_sun = 6e+09
+    gamma_in = 1.01
+    sigma_M = 20
+    a = 0.5
+    R_L = get_R_L(M_BH_sun*M_sun, a)
+    print("R_L = ", R_L)
+    theta_jet = np.deg2rad(10.)
+    theta_obs = np.deg2rad(17.)
+    print("z1 through M,a = ", get_z1_Ma(gamma_in, sigma_M, M_BH_sun, a, theta_jet))
+    print("z1 through R_L = ", get_z1_RL(gamma_in, sigma_M, R_L, theta_jet))
+    print("Ang2dist = ", ang_to_dist(z))
+    l_mas = 1
+    print("1 mas corresponds to {} pc of de-projected distance".format(mas_to_pc(l_mas, z, theta_obs)))
+    print("1 mas corresponds to {} pc of de-projected distance".format(l_mas*u.mas.to(u.rad)*D/np.sin(theta_obs)))
+
+    # post_samples = np.loadtxt("Release/posterior_sample_each10.txt")
+    # z1_proj_mas_samples = np.exp(post_samples[:, 6])
+    # fig, axes = plt.subplots(1, 1)
+    # axes.hist(z1_proj_mas_samples, bins=30)
+    # axes.set_xlabel(r"$z_{\rm 1,proj}$, mas")
+    # plt.show()
+    #
+    # mad = median_abs_deviation(z1_proj_mas_samples)
+    # sigma_mad = 1.4826*mad
+    # sigma_raw = np.std(z1_proj_mas_samples)
+    # r_br = np.median(z1_proj_mas_samples)
+    # print(r_br, sigma_raw, sigma_mad)
---
+++ inference_dnest4.py
@@ -0,0 +1,135 @@
+import sys
+import os
+import dnest4
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import median_abs_deviation
+import corner
+from astropy import units as u, constants as const
+from inference import get_R_g, get_z1_Ma, M_sun
+sys.path.insert(0, '/home/ilya/github/dnest4postprocessing')
+from postprocess import postprocess
+
+
+stack_post_samples = np.loadtxt("Release/posterior_sample_each10.txt")
+z1_proj_mas_samples = np.exp(stack_post_samples[:, 6])
+r0_proj_mas_samples = np.exp(stack_post_samples[:, 4])
+z1_proj_mas_samples = z1_proj_mas_samples + r0_proj_mas_samples
+z1_obs = np.median(z1_proj_mas_samples)
+mad = median_abs_deviation(z1_proj_mas_samples)
+print(z1_obs)
+z1_sigma = 1.4826*mad
+M_BH_sun = 6.5e+09
+M_BH = M_BH_sun*M_sun
+
+class Model(object):
+
+    def __init__(self):
+        self.rad_to_mas = u.rad.to(u.mas)
+
+    def from_prior(self):
+        lg_sigma_M = np.random.normal(1, 0.5)
+        # lg_R_L_rg = np.random.normal(3, 2)
+        M_BH_sun = np.random.normal(6.5, 0.9)
+        D_Mpc = np.random.normal(16.8, 0.8)
+        theta_obs_deg = np.random.normal(17, 2)
+        a = np.random.uniform(0, 1)
+        return np.array([lg_sigma_M, M_BH_sun, D_Mpc, theta_obs_deg, a])
+
+    def perturb(self, params):
+        """
+        Unlike in C++, this takes a numpy array of parameters as input,
+        and modifies it in-place. The return value is still logH.
+        """
+        logH = 0.0
+        which = np.random.randint(5)
+
+        # lg_sigma_M
+        if which == 0:
+            logH -= -0.5*((params[which] - 1.)/0.5)**2
+            params[which] += 0.5*self.randh()
+            logH += -0.5*((params[which] - 1.)/0.5)**2
+        # M_BH_sun
+        elif which == 1:
+            logH -= -0.5*((params[which] - 6.5)/0.9)**2
+            params[which] += 0.9*self.randh()
+            logH += -0.5*((params[which] - 6.5)/0.9)**2
+        # D
+        elif which == 2:
+            logH -= -0.5*((params[which] - 16.8)/0.8)**2
+            params[which] += 0.8*self.randh()
+            logH += -0.5*((params[which] - 16.8)/0.8)**2
+        # theta_obs_deg
+        elif which == 3:
+            logH -= -0.5*((params[which] - 17.)/2.)**2
+            params[which] += 2.*self.randh()
+            logH += -0.5*((params[which] - 17.)/2.)**2
+        else:
+            a = params[which]
+            a += 1.0*self.randh()
+            a = self.wrap(a, 0, 1)
+            params[which] = a
+
+        return logH
+
+    def log_likelihood(self, params):
+        """
+        Gaussian sampling distribution.
+        """
+        lg_sigma_M, M_BH_sun, D, theta_obs_deg, a = params
+        # In pc
+        z1_model_pc = get_z1_Ma(1.1, 10**lg_sigma_M, M_BH_sun*1e+09, a, np.deg2rad(10.))
+        # In mas
+        z1_model_mas = self.rad_to_mas*(z1_model_pc*np.sin(np.deg2rad(theta_obs_deg))/(1e+06*D))
+        # return -0.5*data.shape[0]*np.log(2*np.pi*var) \
+        #     - 0.5*np.sum((data[:,1] - (m*data[:,0] + b))**2)/var
+        return -0.5*np.log(2*np.pi*z1_sigma**2) - 0.5*(z1_obs - z1_model_mas)**2/z1_sigma/z1_sigma
+
+    def randh(self):
+        """
+        Generate from the heavy-tailed distribution.
+        """
+        a = np.random.randn()
+        b = np.random.rand()
+        t = a/np.sqrt(-np.log(b))
+        n = np.random.randn()
+        return 10.0**(1.5 - 3*np.abs(t))*n
+
+    def wrap(self, x, a, b):
+        assert b > a
+        return (x - a)%(b - a) + a
+
+
+# Create a model object and a sampler
+model = Model()
+sampler = dnest4.DNest4Sampler(model,
+                               backend=dnest4.backends.CSVBackend(".",
+                                                                  sep=" "))
+
+# Set up the sampler. The first argument is max_num_levels
+gen = sampler.sample(max_num_levels=15, num_steps=10000, new_level_interval=10000,
+                     num_per_step=10000, thread_steps=100,
+                     num_particles=5, lam=10, beta=100, seed=1234)
+
+# Do the sampling (one iteration here = one particle save)
+for i, sample in enumerate(gen):
+    print("# Saved {k} particles.".format(k=(i+1)))
+
+fitted_file = "/home/ilya/github/stack_fitter/posterior_sample.txt"
+run_dir = "/home/ilya/github/stack_fitter"
+logZ, _, _ = postprocess(plot=True,
+                         sample_file=os.path.join(run_dir, 'sample.txt'),
+                         level_file=os.path.join(run_dir, 'levels.txt'),
+                         sample_info_file=os.path.join(run_dir, 'sample_info.txt'),
+                         post_sample_file=os.path.join(run_dir, "posterior_sample.txt"))
+
+post_samples = np.loadtxt(fitted_file)
+n_post = len(post_samples)
+post_samples_use = np.vstack((post_samples[:, 0], post_samples[:, 4])).T
+fig = corner.corner(post_samples_use, show_titles=True, labels=[r"$\lg{\sigma_{\rm M}}$",
+                                                            # r"${M_{\rm BH}}$, [$10^9 M_{\rm sun}$]",
+                                                            # r"$D$, [Mpc]",
+                                                            # r"$\theta_{\rm obs}$, $^{\circ}$",
+                                                                r"$a$"], quantiles=[0.16, 0.50, 0.84])
+fig.savefig("inference_sigma_a.png", bbox_inches="tight", dpi=300)
+plt.show()
